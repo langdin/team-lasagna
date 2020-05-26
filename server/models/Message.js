0b0411731b61c1ad9f0965f134b57c97e76931a2
Din Lanh <langdinhieu@gmail.com>
2020-05-26 17:22:37 -0400

    added createdAt field to message model

--- server/models/Message.js
+++ server/models/Message.js
@@ -14,6 +14,10 @@ const messageSchema = new mongoose.Schema({
     text: {
         type: String,
         required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
 
