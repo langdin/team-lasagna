9d5da1f99a6c8999eadf220722778aaef725e53c
Din Lanh <langdinhieu@gmail.com>
2020-05-31 22:22:57 -0400

    added ability to create conversations

--- client/src/pages/Messages.js
+++ client/src/pages/Messages.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
 import "react-credit-cards/es/styles-compiled.css";
@@ -15,6 +16,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Messages() {
   const classes = useStyles();
+  const location = useLocation();
   const [recipient, setRecipient] = useState({});
   const [contacts, setContacts] = useState([]);
   const [messages, setMessages] = useState([]);
@@ -96,6 +98,9 @@ export default function Messages() {
     socket.on("new message", (msg) => {
       setMessages([...messages, msg]);
     });
+    if (location.profile) {
+      createConversation();
+    }
     return () => {
       socket.removeListener("new message");
     };
@@ -105,7 +110,26 @@ export default function Messages() {
     getContacts();
     getMessages();
     scrollToBottom();
-  }, [contacts.length, messages.length, conversation]);
+  }, [contacts.length, messages.length, conversation, recipient]);
+
+  const createConversation = async () => {
+    try {
+      const newConversation = await axios.post(
+        `http://localhost:3001/conversation/`,
+        {
+          recipientId: location.profile._id,
+          senderId: JSON.parse(localStorage.getItem("profile"))._id,
+        }
+      );
+      if (newConversation.data) {
+        setConversation(newConversation.data.conversation._id);
+        setRecipient(location.profile);
+      }
+      console.log(newConversation.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
